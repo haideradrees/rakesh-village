8c82e4a731041c23e5ad6dfd6311ddc08123e0b2
Haider Adrees <haideradrees7@gmail.com>
2024-11-03 01:38:49 +0500

    Add Tool tips

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { useState, useRef } from 'react';
-import { Carousel, Dropdown, Nav, Navbar, NavDropdown, } from 'react-bootstrap';
-import Image from 'next/image';
+import { Overlay, Tooltip } from 'react-bootstrap';
 import BackgroundMusicPlayer from '@/components/BackgroundMusicPlayer';
 
 export default function Index() {
+    // State and refs for each element
+    const [show1, setShow1] = useState(false);
+    const [show2, setShow2] = useState(false);
+    const [show3, setShow3] = useState(false);
+    const target1 = useRef(null);
+    const target2 = useRef(null);
+    const target3 = useRef(null);
 
     return (
         <div className='frame'>
@@ -11,20 +17,72 @@ export default function Index() {
                 <video autoPlay loop muted>
                     <source src="/video.mp4" type="video/mp4" />
                 </video>
-                <a className="pulseWrapper1" target='_blank'>
+
+                {/* First element with tooltip on hover */}
+                <a
+                    className="pulseWrapper1"
+                    ref={target1}
+                    onMouseEnter={() => setShow1(true)}
+                    onMouseLeave={() => setShow1(false)}
+                >
                     <div className="fixedCircle"></div>
                     <div className="pulseLoader"></div>
                 </a>
-                <a className="pulseWrapper2" target='_blank'>
+                {target1.current && (
+                    <Overlay target={target1.current} show={show1} placement="right">
+                        {props => (
+                            <Tooltip id="tooltip1" {...props} className="custom-tooltip">
+                                Coming Soon
+                            </Tooltip>
+                        )}
+                    </Overlay>
+                )}
+
+                {/* Second element with tooltip on hover */}
+                <a
+                    className="pulseWrapper2"
+                    ref={target2}
+                    target="_blank"
+                    onMouseEnter={() => setShow2(true)}
+                    onMouseLeave={() => setShow2(false)}
+                >
                     <div className="fixedCircle"></div>
                     <div className="pulseLoader"></div>
                 </a>
-                <a className="pulseWrapper3" href="https://home.rakeshvillage.fun/" target='_blank'>
+                {target2.current && (
+                    <Overlay target={target2.current} show={show2} placement="right">
+                        {props => (
+                            <Tooltip id="tooltip2" {...props} className="custom-tooltip">
+                                Coming Soon
+                            </Tooltip>
+                        )}
+                    </Overlay>
+                )}
+
+                {/* Third element with tooltip on hover */}
+                <a
+                    className="pulseWrapper3"
+                    ref={target3}
+                    href="https://home.rakeshvillage.fun/"
+                    target="_blank"
+                    onMouseEnter={() => setShow3(true)}
+                    onMouseLeave={() => setShow3(false)}
+                >
                     <div className="fixedCircle"></div>
                     <div className="pulseLoader"></div>
                 </a>
+                {target3.current && (
+                    <Overlay target={target3.current} show={show3} placement="right">
+                        {props => (
+                            <Tooltip id="tooltip3" {...props} className="custom-tooltip">
+                                Village
+                            </Tooltip>
+                        )}
+                    </Overlay>
+                )}
             </div>
-            <div className='scoll-right'>
+
+            <div className='scroll-right'>
                 <span>Scroll</span>
                 <div className="arrow-box">
                     <div className="arrow"></div>
